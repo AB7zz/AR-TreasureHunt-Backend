d975150dd98b2ad81b99d28c8b897d39012d4cbf
AB7zz <abhinavcv007@gmail.com>
2023-11-11 10:29:29 +0530

    few chages

--- controllers/user/insertUser.js
+++ controllers/user/insertUser.js
@@ -2,7 +2,7 @@ import db from '../../config/firebase-config.js'
 
 const insertUser = async(req, res) => {
     try {
-        const uid = req.headers.authorization
+        const uid = req.headers.uid
         const userRef = db.collection('users').doc(uid)
         const user = await userRef.get()
         if (user.exists) {
@@ -17,7 +17,7 @@ const insertUser = async(req, res) => {
         
         await userRef.set(data)
         
-        return res.status(200).json({success: 1, message: 'User inserted', result})
+        return res.status(200).json({success: 1, message: 'User inserted'})
     } catch (error) {
         console.log(error)
         return res.json({success: -1, message: 'Some error occurred'})
--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import express from 'express'
 import dotenv from 'dotenv'
 import cors from 'cors'
 import routes from './routes/index.js';
-// import Middleware from './middleware/index.js'
+import Middleware from './middleware/index.js'
 
 dotenv.config()
 

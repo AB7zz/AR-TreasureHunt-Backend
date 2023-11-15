8e52790e053d52817f20e515e6780500a6a5dea2
i0dheerajdileep <i.dheerajdileep@gmail.com>
2023-11-15 15:49:32 +0530

    loc vals

--- controllers/location/fetchLocation.js
+++ controllers/location/fetchLocation.js
@@ -17,32 +17,33 @@ const fetchLocation = async(req, res) => {
 
         const locations = [
             {
-                name: '1',
+                level: '1',
                 x1: ONE_X_1,
                 x2: ONE_X_2
             },
             {
-                name: '2',
+                level: '2',
                 x1: TWO_X_1,
                 x2: TWO_X_2
             },
             {
-                name: '3',
+                level: '3',
                 x1: THREE_X_1,
                 x2: THREE_X_2
             },
             {
-                name: '4',
+                level: '4',
                 x1: FOUR_X_1,
                 x2: FOUR_X_2
             },
             {
-                name: '5',
+                level: '5',
                 x1: FIVE_X_1,
                 x2: FIVE_X_2
             }
         ]
-        return res.status(200).json({success: 1, locations})
+        // console.log("dkjfkjbe")
+        return res.status(200).json({locations})
     } catch (error) {
         console.log(error)
         return res.json({success: -1, message: 'Some error occurred'})

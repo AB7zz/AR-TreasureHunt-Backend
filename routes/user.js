97347b6fb4527d8f382e1e497768cfcfb60f5b64
AB7zz <abhinavcv007@gmail.com>
2023-11-15 17:48:12 +0530

    login route

--- routes/user.js
+++ routes/user.js
@@ -1,9 +1,11 @@
 import express from 'express';
 import userDetails from '../controllers/user/userDetails.js'
 import insertUser from '../controllers/user/insertUser.js'
+import loginUser from '../controllers/user/loginUser.js'
 const router = express.Router()
 
 router.post('/user', insertUser)
+router.post('/login', loginUser)
 router.get('/user/:uid', userDetails)
 
 export default router   

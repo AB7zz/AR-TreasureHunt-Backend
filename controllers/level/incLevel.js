20b077873b8ae6193c3b8940144dbd6bc0409f19
AB7zz <abhinavcv007@gmail.com>
2023-11-20 11:15:55 +0530

    timestamp

--- controllers/level/incLevel.js
+++ controllers/level/incLevel.js
@@ -9,14 +9,19 @@ const incLevel = async(req, res) => {
             return res.status(404).json({success: 0, message: 'User not found'})
         }
         
+        let timestamps
+        if(user.data().timestamps){
+            timestamps = [...user.data().timestamps, req.body.timestamp]
+        }else{
+            timestamps = [req.body.timestamp]
+        }
         const data = {
             level: user.data().level + 1,
-            timestamps: [...user.data().timestamps, req.body.timestamp]
+            timestamps
         }
         const result = await userRef.update(data)
         return res.status(200).json({success: 1, message: 'Level incremented'})
     } catch (error) {
-        console.log(error)
         return res.json({success: -1, message: 'Some error occurred'})
     }
 }
